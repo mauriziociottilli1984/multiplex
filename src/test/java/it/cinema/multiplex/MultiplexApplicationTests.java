3f539af6b9649b7e8dc67248b48b185745a0f758
Maurizio Ciottilli <m.ciottilli@gmail.com>
2020-09-29 16:52:19 +0200

    first commit

---
+++ src/test/java/it/cinema/multiplex/MultiplexApplicationTests.java
@@ -0,0 +1,13 @@
+package it.cinema.multiplex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiplexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
